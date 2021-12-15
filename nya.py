1ba6358122ae9fc92e5301780ba29ad1b853ce9f
PhaseRush <PhaseAnsible@gmail.com>
2021-12-15 14:33:11 -0800

    refactor and add typing nya

--- nya.py
+++ nya.py
@@ -1,31 +1,5 @@
 import sys
-from typing import List
-
-
-def _find_getch():
-    try:
-        import termios
-    except ImportError:
-        # Non-POSIX. Return msvcrt's (Windows') getch.
-        import msvcrt
-        return msvcrt.getch
-
-    # POSIX system. Create and return a getch that manipulates the tty.
-    import sys, tty
-    def _getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-    return _getch
-
-
-getch = _find_getch()
+from typing import List, Callable, Tuple
 
 braces: dict[int, int] = {}
 
@@ -83,7 +57,7 @@ def sinp(data: List[int], data_ptr: int, instr_ptr: int) -> (int, int):
     return data_ptr, instr_ptr
 
 
-OP_CODES = {
+OP_CODES: dict[str, [Callable[[List[int], int], int], Tuple[int, int]]] = {
     "Nya": prev,  # < prev
     "nyA": next,  # > next
     "nya": incr,  # + incr
@@ -114,8 +88,8 @@ def run(filename: str):
     data = [0]
     data_ptr = 0
     while instr_ptr < len(instructions):
-        func = OP_CODES[instructions[instr_ptr]]
-        data_ptr, instr_ptr = func(data, data_ptr, instr_ptr)
+        op = OP_CODES[instructions[instr_ptr]]
+        data_ptr, instr_ptr = op(data, data_ptr, instr_ptr)
         instr_ptr += 1
 
 
@@ -125,3 +99,29 @@ if __name__ == '__main__':
         print(f"You fucked up. Run your catgirls properly baka.")
     else:
         run(sys.argv[1])
+
+
+def _find_getch():
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
+
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+
+
+getch = _find_getch()
